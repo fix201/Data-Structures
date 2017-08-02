49dda357fcc2c0f5c2018465131a2e0b8601e5ee
Olufisayo Joseph Ayodele <fix201@users.noreply.github.com>
2017-08-02 16:38:34 -0500

    Add files via upload

---
+++ InteractiveConstruct.java
@@ -0,0 +1,14 @@
+package dataStructures;
+
+public class InteractiveConstruct{
+
+	public InteractiveConstruct(){
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+
+	}
+
+}
